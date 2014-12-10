d46b3bc5814bea73069a778eb4aa678a7ce5bf44
Nick McDonnough <nickmcd@gmail.com>
2014-12-10 09:35:08 -0600

    Add instructions to users repo to provide some guidance

--- lib/blogtastic/repos/users_repo.rb
+++ lib/blogtastic/repos/users_repo.rb
@@ -1,14 +1,28 @@
 module Blogtastic
   class UsersRepo
+    # find a user by user ID. Intended to be used when
+    # someone is already authenticated. We keep their
+    # user id in the cookie.
     def self.find db, user_id
       sql = %q[SELECT * FROM users WHERE id = $1]
       result = db.exec(sql, [user_id])
       result.first
     end
 
+    # find user by username. Intended to be used when
+    # someone tries to sign in.
+    def self.find_by_name db, username
+      sql = %q[SELECT * FROM users WHERE username = $1]
+      result = db.exec(sql, [username])
+      result.first
+    end
+
+    # when someone signs up use this method to save their
+    # information in the db. we're not encrypting passwords.
     def self.save db, user_data
-      # TODO: save user's info when they sign up.
-      #       should include username, password, email
+      sql = %q[INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *]
+      result = db.exec(sql, [user_data[:username], user_data[:password]])
+      result.first
     end
   end
 end
